e3f5ceb93c8d1d54760b32d5997a51ccbc89b870
赵庆华 <qinghua.zhao@zymobi.com>
2016-07-17 22:38:58 +0800

    add image control

--- assets/designer/controls/view/control/control.image.js
+++ assets/designer/controls/view/control/control.image.js
@@ -1,10 +1,19 @@
 jQuery(function($) {
-    var Template = loadTemplate("../assets/designer/controls/template/control/search.html");
+    var Template = loadTemplate("../assets/designer/controls/template/control/image.html");
     var View = Backbone.Designer.View.extend({//options...
         initialize : function(option) {
+            var self = this;
             this.render();
             Backbone.Designer.View.prototype.initialize.apply(this, arguments);
-
+            this.listenTo(this.model,"change:src",function(data){
+                this.$el.attr("src",data.changed.src);
+            })
+            this.listenTo(this.model,"change:width",function(data){
+                this.$el.attr("width",data.changed.width);
+            })
+            this.listenTo(this.model,"change:height",function(data){
+                this.$el.attr("height",data.changed.height);
+            })
         },
         template : Template, //VIEW对应的模板
         render : function() {
@@ -19,16 +28,32 @@ jQuery(function($) {
 
     var Config = Backbone.Designer.Config.extend({
         initialize : function() {
-            this.set("type", "Search");
+            this.set("type", "Image");
             Backbone.Designer.Config.prototype.initialize.apply(this, arguments);
-        }
+            this.set("width","auto");
+            this.set("height","auto");
+            this.set("src","");
+        },
+        extOptions:[{
+            type : "input",
+            title : "图片路径",
+            name : "src"
+        },{
+            type : "input",
+            title : "图片宽度",
+            name : "width"
+        },{
+            type : "input",
+            title : "图片高度",
+            name : "height"
+        }]
     })
 
     window.desUIControlsListViewInstance.register({
-        uuid : "021c646d-17c9-40ae-a351-08aa146c2714",
-        name : "Search",
-        tip : "",
-        icon : "UI设计器_53.png"
+        uuid : "4b78c7ac-5d40-4d1b-b5e6-740314738146",
+        name : "Image",
+        tip : "图片组件，用于通过img标签显示图片",
+        icon : "Img.png"
     }, {
         View : View,
         Template : Template,
